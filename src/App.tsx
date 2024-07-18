6baddaa3109bdca9bbc896f2b29bd20f4022ba69
alaa ben mohamed <106694845+alaabenmohamed@users.noreply.github.com>
2024-07-18 11:22:27 +0100

    feat: add test for `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Title avec h1</h1>
+      <h2>Title avec h2</h2>
+      <p>Tous les champs sont obligatoires</p>
+      <span title="close">X</span>
+      <img
+        src="https://th.bing.com/th/id/OIP.o8ubNKazDVWgRKfCY1iB7gHaHa?w=160&h=180&c=7&r=0&o=5&pid=1.7"
+        alt="react logo"
+      />
+      <div data-testid="custom-element">Custom HTML element</div>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Fullname"
+            value="Alaa"
+            onChange={() => {}}
+          />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea id="bio" />
+        </div>
+        <div>
+          <label htmlFor="job-location">Job location</label>
+          <select id="job-location">
+            <option value="">Select a country</option>
+            <option value="Tu">Tunisie</option>
+            <option value="US">United States</option>
+            <option value="CA">Canada</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" /> J’accepte les conditions
+          </label>
+        </div>
+        <button disabled>Envoyer</button>
+      </form>
+    </>
   );
 }
 
-export default App;
