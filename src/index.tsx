29c78b078067d14144bc72388dfd6dd5e2c8789d
alaa ben mohamed <106694845+alaabenmohamed@users.noreply.github.com>
2024-07-18 11:28:28 +0100

    feat: add all components in `index.tsx`

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { Counter } from "./components/counter/counter";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Counter />
+    {/* <CounterTwo {...{ count :1}} /> */}
   </React.StrictMode>
 );
 
